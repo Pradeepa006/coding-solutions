61f1082b6ad635d75c5112f0674dc81d7561dbea
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-15 20:44:52 +0530

    ✅ FLIP2K | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/FLIP2K/solution.java
+++ codechef/medium/FLIP2K/solution.java
@@ -1,12 +1,39 @@
 import java.util.*;
 import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Codechef {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            String s = sc.next();
+
+            int zeros = 0;
+            for (int i = 0; i < N; i++) {
+                if (s.charAt(i) == '0')
+                    zeros++;
+            }
+
+            int ones = N - zeros;
+
+            if (zeros < K || ones < K) {
+                System.out.println(s);
+            } else {
+                StringBuilder ans = new StringBuilder();
+
+                for (int i = 0; i < zeros; i++)
+                    ans.append('0');
+
+                for (int i = 0; i < ones; i++)
+                    ans.append('1');
+
+                System.out.println(ans);
+            }
+        }
+    }
 }
