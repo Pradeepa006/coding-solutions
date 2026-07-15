c5aeff662b7e65218630eafba4522e04f17ccd84
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-15 20:26:51 +0530

    ✅ ALTARR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ codechef/medium/ALTARR/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void minMoves()
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		for(int i = 0 ; i < N ; i++) {
+		    int n = scan.nextInt();
+		    minMoves(n);
+		}
+      
+	}
+}
