0fdca6dd273a95b6759e41123119b1131a79346b
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-15 20:34:28 +0530

    ✅ ALTARR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/ALTARR/solution.java
+++ codechef/medium/ALTARR/solution.java
@@ -2,18 +2,40 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-    public static void minMoves()
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		for(int i = 0 ; i < N ; i++) {
-		    int n = scan.nextInt();
-		    minMoves(n);
-		}
-      
-	}
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+
+        while (T-- > 0) {
+            int n = scan.nextInt();
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+
+            int p1 = 0; // even index -> even, odd index -> odd
+            int p2 = 0; // even index -> odd, odd index -> even
+
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    if (arr[i] % 2 == 0)
+                        p1++;
+                    else
+                        p2++;
+                } else {
+                    if (arr[i] % 2 != 0)
+                        p1++;
+                    else
+                        p2++;
+                }
+            }
+
+            System.out.println(Math.min(p1, p2));
+        }
+
+        scan.close();
+    }
 }
