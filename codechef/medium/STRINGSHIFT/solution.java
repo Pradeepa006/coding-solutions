765a4c7e372740d4285705922f3c8621f4b1de71
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-15 20:46:03 +0530

    ✅ STRINGSHIFT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/STRINGSHIFT/solution.java
+++ codechef/medium/STRINGSHIFT/solution.java
@@ -1,12 +1,29 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
 
-	}
+class Codechef {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            char[] s = sc.next().toCharArray();
+
+            int i = 0;
+
+            // Find first 'z'
+            while (i < N && s[i] != 'z')
+                i++;
+
+            // Convert consecutive z's to a's
+            while (i < N && s[i] == 'z') {
+                s[i] = 'a';
+                i++;
+            }
+
+            System.out.println(new String(s));
+        }
+    }
 }
