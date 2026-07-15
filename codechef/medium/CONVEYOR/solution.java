791ce8c48a0f29f2b3373c88df747a106e3cbd9a
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-15 20:38:46 +0530

    ✅ CONVEYOR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/CONVEYOR/solution.java
+++ codechef/medium/CONVEYOR/solution.java
@@ -1,12 +1,29 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+class Codechef {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+        int T = scan.nextInt();
 
-	}
+        while (T-- > 0) {
+            int N = scan.nextInt();
+            int P = scan.nextInt();
+
+            String S = scan.next();
+
+            int left = 0;
+            for (int i = P - 1; i >= 0; i--) {
+                if (S.charAt(i) == 'R')
+                    left++;
+            }
+
+            int right = 0;
+            for (int i = P - 1; i < N; i++) {
+                if (S.charAt(i) == 'L')
+                    right++;
+            }
+
+            System.out.println(Math.min(left, right));
+        }
+    }
 }
