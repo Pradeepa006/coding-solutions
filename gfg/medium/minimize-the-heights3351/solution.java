cf5498bc5873f7eb112a49db66678369c84ab26a
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-17 10:54:39 +0530

    ✅ Minimize the Heights II | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-17

---
+++ gfg/medium/minimize-the-heights3351/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMinDiff(int[] arr, int k) {
+        // code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int ans = arr[n - 1] - arr[0];
+        for(int  i = 0 ; i < n - 1 ; i++) {
+            if(arr[i + 1] - k < 0)
+                continue;
+            int small = Math.min(arr[0] + k , arr[i + 1] - k);
+            int large = Math.max(arr[i] + k , arr[n - 1] - k);
+            ans = Math.min(ans , large - small);
+        }
+        return ans;
+    }
+}
