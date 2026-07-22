2378ba1636b90289e2ada0830bbe409bb5e5f97b
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-22 13:36:06 +0530

    ✅ Indexes of Subarray Sum | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ gfg/medium/subarray-with-given-sum-1587115621/solution.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> idx = new ArrayList<>();
+        
+        int start = 0 ;
+        //int end = 0 ;
+        
+        int sum = 0;
+        
+        for(int end = 0 ; end < n ; end++) {
+            sum += arr[end];
+            
+            while(sum > target) {
+                sum -= arr[start];
+                start++;
+            }
+            
+            if(sum == target) {
+                idx.add(start + 1);
+                idx.add(end + 1);
+                return idx;
+            }
+        }
+        
+        idx.add(-1);
+        return idx;
+    }
+}
