c234dcd984156b467e5e124770fc0cdb979caf39
Pradeepa S <155996905+Pradeepa006@users.noreply.github.com>
2026-08-04 10:08:27 +0530

    ✅ Generate Parentheses | Medium | Java | Runtime: 2 ms | Memory: 42.5 MB | 2026-08-04

--- leetcode/medium/0022-generate-parentheses/solution.java
+++ leetcode/medium/0022-generate-parentheses/solution.java
@@ -6,13 +6,13 @@ class Solution {
         return li;
     }
     private static void backtrack(List<String> li , String s , int open , int close , int max){
-        if(max == s.length() * 2) {
+        if(max * 2 == s.length()) {
             li.add(s);
             return;
         }
         if(open < max ) 
-            backtrack(li , "(" , open + 1 , close , max);
-        if(close < max)
-            backtrack(li , ")" , open , close + 1 , max);
+            backtrack(li , s+"(" , open + 1 , close , max);
+        if(close < open)
+            backtrack(li , s+")" , open , close + 1 , max);
     }
 }
